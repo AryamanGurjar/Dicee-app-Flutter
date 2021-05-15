18a39ba6a66db1927a640092525abbad589855f1
Aryaman Gurjar <aryamangurjar6@gmail.com>
2021-05-15 22:14:53 +0530

    A two Dice app  created using Flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -6,7 +8,17 @@ void main() {
       home: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
-          title: Text('Dicee'),
+          title: Center(
+            child: Text(
+              ':-)    HAPPY DICE    (-:',
+              style: TextStyle(
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
+                fontFamily: 'Itim',
+                fontSize: 26.0,
+              ),
+            ),
+          ),
           backgroundColor: Colors.red,
         ),
         body: DicePage(),
@@ -15,9 +27,80 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  const DicePage({Key key}) : super(key: key);
+
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftdicenumber = 1;
+  int rightdicenumber = 1;
+  int total;
+  both() {
+    rightdicenumber = Random().nextInt(6) + 1;
+    leftdicenumber = Random().nextInt(6) + 1;
+    total = leftdicenumber + rightdicenumber;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Column(
+        children: [
+          SizedBox(
+            height: 10.0,
+          ),
+          Expanded(
+            child: FlatButton(
+              child: Padding(
+                padding: const EdgeInsets.all(24.0),
+                child: Image.asset('images/dice$leftdicenumber.png'),
+              ),
+              onPressed: () {
+                setState(() {
+                  both(); //1 - 6
+                });
+              },
+            ),
+          ),
+          SizedBox(
+            height: 10.0,
+          ),
+          Container(
+            child: Text(
+              'Press the dice to take your turn\n\t\t\t\t\t\t\t\t\t\t\t\t\t\tTOTAL = $total',
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                fontFamily: 'Itim',
+                fontSize: 19.0,
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 3.0,
+          ),
+          Expanded(
+            child: FlatButton(
+              child: Padding(
+                padding: const EdgeInsets.all(24.0),
+                child: Image.asset('images/dice$rightdicenumber.png'),
+              ),
+              onPressed: () {
+                setState(() {
+                  both();
+                  //if +1 is not added than it will show number from 0 to 5 and also dart:math lib is added
+                });
+              },
+            ),
+          ),
+          SizedBox(
+            height: 10.0,
+          ),
+        ],
+      ),
+    );
   }
 }
